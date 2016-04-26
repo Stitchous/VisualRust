a842591730df7600588f6eb3e8a8716728f0676a
Alexey Subbotin <alexeysb@gmail.com>
2016-04-27 02:52:33 +0300

    language service registration added

--- VisualRust/RustLanguage.cs
+++ VisualRust/RustLanguage.cs
@@ -5,12 +5,66 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.Package;
 using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio;
 
 namespace VisualRust
 {
     internal sealed class RustLanguage : LanguageService
     {
         private LanguagePreferences preferences;
+
+        internal class StubScanner : IScanner
+        {
+            private IVsTextBuffer m_buffer;
+            string m_source;
+
+            public StubScanner(IVsTextBuffer buffer)
+            {
+                m_buffer = buffer;
+            }
+
+            bool IScanner.ScanTokenAndProvideInfoAboutIt(TokenInfo tokenInfo, ref int state)
+            {
+                tokenInfo.Type = TokenType.Unknown;
+                tokenInfo.Color = TokenColor.Text;
+                return false;
+            }
+
+            void IScanner.SetSource(string source, int offset)
+            {
+                m_source = source.Substring(offset);
+            }
+        }
+
+        internal class StubAuthoringScope : AuthoringScope
+        {
+            public override string GetDataTipText(int line, int col, out TextSpan span)
+            {
+                span = new TextSpan();
+                return null;
+            }
+
+            public override Declarations GetDeclarations(IVsTextView view,
+                                                         int line,
+                                                         int col,
+                                                         TokenInfo info,
+                                                         ParseReason reason)
+            {
+                return null;
+            }
+
+            public override string Goto(VSConstants.VSStd97CmdID cmd, IVsTextView textView, int line, int col, out TextSpan span)
+            {
+                span = new TextSpan();
+                return null;
+            }
+
+            public override Methods GetMethods(int line, int col, string name)
+            {
+                return null;
+            }
+        }
+
 
         public override LanguagePreferences GetLanguagePreferences()
         {
@@ -28,6 +82,7 @@ namespace VisualRust
                 preferences.AutoOutlining = false;
                 preferences.MaxRegionTime = 2000;
                 preferences.ShowNavigationBar = true;
+                preferences.EnableFormatSelection = true;
 
                 preferences.AutoListMembers = false;
                 preferences.EnableQuickInfo = false;
@@ -43,7 +98,7 @@ namespace VisualRust
 
         public override IScanner GetScanner(IVsTextLines buffer)
         {
-            return null;
+            return new StubScanner(buffer);
         }
 
         public override string Name
@@ -53,7 +108,12 @@ namespace VisualRust
 
         public override AuthoringScope ParseSource(ParseRequest req)
         {
-            return null;
+            return new StubAuthoringScope();
+        }
+
+        public override Source CreateSource(IVsTextLines buffer)
+        {
+            return new RustSource(this, buffer, base.GetColorizer(buffer));
         }
     }
 }
---
+++ VisualRust/RustSource.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.Package;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace VisualRust
+{
+    internal class RustSource : Source
+    {
+        public RustSource(LanguageService service, IVsTextLines textLines, Colorizer colorizer) : base(service, textLines, colorizer)
+        {
+        }
+
+        public override void ReformatSpan(EditArray mgr, TextSpan span)
+        {
+            base.ReformatSpan(mgr, span);
+        }
+    }
+}
