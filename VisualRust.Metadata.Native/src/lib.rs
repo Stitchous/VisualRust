b13b259b1e10c0bedf6cd992a9d852ef3078a91d
Stitchous <alexander.amrilloev@gmail.com>
2016-04-27 19:23:17 +0300

    working on crate metadata reading

--- VisualRust.Metadata.Native/src/lib.rs
+++ VisualRust.Metadata.Native/src/lib.rs
@@ -1,3 +1,9 @@
-#[test]
-fn it_works() {
+#![feature(rustc_private)] 
+
+extern crate rustc_metadata;
+
+#[no_mangle]
+pub fn get_crate_name(data: &[u8]) -> *const u8
+{
+    rustc_metadata::decoder::get_crate_name(data).as_ptr()
 }
